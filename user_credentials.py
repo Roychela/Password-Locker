3375d44ba4db6f4f6dd984ae5f4060de6100e167
Roy Chela <roychela@gmail.com>
2019-06-16 20:28:28 +0300

    Add docstrings to document User class methods and TestUser class tests

--- user_credentials.py
+++ user_credentials.py
@@ -11,5 +11,9 @@ class User:
         self.username = username
         self.password = password
     def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+
         User.user_list.append(self)
     
--- user_credentials_test.py
+++ user_credentials_test.py
@@ -2,16 +2,33 @@ import unittest
 from user_credentials import User
 class TestUser(unittest.TestCase):
     def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
         self.new_user = User('Js', "123abc")
     def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
         User.user_list = []
     def test__init__(self):
+        '''
+        test__init__ test case to test if the object is initialized properly
+        '''
         self.assertEqual(self.new_user.username,"Js")
         self.assertEqual(self.new_user.password,"123abc")
     def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+        the user_list
+        '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
     def test_save_multiple_users(self):
+        '''
+        test_save_multiple_users to check if we can save multiple user
+        objects to our user_list
+        '''
         self.new_user.save_user()
         test_user = User('Test', 'User123')
         test_user.save_user()
